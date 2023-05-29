c0764b2f697138a727bacf7214f38d01965c1e67
maartenhoebekePXL <maarten.hoebeke@student.pxl.be>
2023-05-29 23:58:34 +0200

    responsive nav bar js

--- assets/js/Producten.js
+++ assets/js/Producten.js
@@ -1,3 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links a");
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.2s ease forwards ${
+                    index / 7 + 0.5
+                }s `;
+            }
+        });
+        burger.classList.toggle("toggle");
+    });
+    //
+};
+
+navSlide();
+
 let navToggle = document.querySelector(".navtoggle");
 let navWrapper = document.querySelector(".navwrapper");
 
